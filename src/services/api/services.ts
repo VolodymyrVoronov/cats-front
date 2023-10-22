aabb679348a0096bb1b555d5a59644ca55b57e25
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-10-22 17:07:15 +0200

    Updated services.

--- src/services/api/services.ts
+++ src/services/api/services.ts
@@ -1,48 +1,15 @@
 import client from './axios-client';
 
-const fetchAllCats = async () => {
-  try {
-    const res = await client.get('/cats');
-    return res.data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
-};
+const fetchAllCats = () => client.get('/cats');
+const fetchCatByID = (id: string) => client.get(`/cats/${id}`);
+const createCat = (data: any) => client.post('/cat', data);
+const deleteCat = (id: string) => client.delete(`/cats/${id}`);
+const updateCat = (id: string, data: any) => client.put(`/cats/${id}`, data);
 
-const fetchCatByID = async (id: string) => {
-  try {
-    const res = await client.get(`/cats/${id}`);
-    return res.data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
+export const CatService = {
+  fetchAllCats,
+  fetchCatByID,
+  createCat,
+  deleteCat,
+  updateCat,
 };
-
-const createCat = async (data: any) => {
-  try {
-    const res = await client.post('/cat', data);
-    return res.data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
-};
-
-const deleteCat = async (id: string) => {
-  try {
-    const res = await client.delete(`/cats/${id}`);
-    return res.data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
-};
-
-const updateCat = async (id: string, data: any) => {
-  try {
-    const res = await client.put(`/cats/${id}`, data);
-    return res.data;
-  } catch (error) {
-    throw new Error((error as Error).message);
-  }
-};
-
-export { fetchAllCats, fetchCatByID, createCat, deleteCat, updateCat };
