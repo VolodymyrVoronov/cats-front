0783e2a4f95f90d1426e01f970d7aff23b7c617e
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-10-24 20:17:02 +0200

    Added animation between pages' transition.

--- src/components/layout/PageLayout/PageLayout.tsx
+++ src/components/layout/PageLayout/PageLayout.tsx
@@ -15,10 +15,29 @@ const PageLayout = ({
   return (
     <motion.div
       className={cn(styles.root, className)}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1 }}
+      initial={{
+        opacity: 0,
+        filter: 'blur(10px)',
+        scale: 0.975,
+      }}
+      exit={{
+        opacity: 0,
+        filter: 'blur(10px)',
+        scale: 0.975,
+        transition: {
+          delay: 0.5,
+          duration: 1,
+        },
+      }}
+      animate={{
+        opacity: 1,
+        filter: 'blur(0px)',
+        scale: 1,
+      }}
+      transition={{
+        duration: 1,
+        ease: 'easeInOut',
+      }}
     >
       {children}
     </motion.div>
